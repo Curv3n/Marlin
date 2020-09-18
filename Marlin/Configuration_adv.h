85e265fb7163bf8815ff4404dfba678d25050438
Curv3n <67075549+Curv3n@users.noreply.github.com>
2020-09-18 15:05:35 +0200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2420,7 +2420,7 @@
    * Use Trinamic's ultra quiet stepping mode.
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
-  #define STEALTHCHOP_XY
+  //#define STEALTHCHOP_XY
   //#define STEALTHCHOP_Z
   //#define STEALTHCHOP_E
 
